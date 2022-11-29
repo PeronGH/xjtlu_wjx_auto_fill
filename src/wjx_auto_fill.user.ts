f5f58430f6f817ba0dab88e81024842c3b8b9ad0
peron <peron_ap@icloud.com>
2022-11-29 22:07:55 +0800

    auto record answer

--- src/wjx_auto_fill.user.ts
+++ src/wjx_auto_fill.user.ts
@@ -1,52 +1,36 @@
 // ==UserScript==
 // @name         WJX Auto Fill For XJTLU
-// @version      1.0.2
+// @version      1.1.0
 // @description  Automatically fill in for XJTLU WJX Surveys
 // @author       Peron
 // @match        https://xjtlusurvey.wjx.cn/vm/*
 // @grant        none
 // ==/UserScript==
 
-const defaultAnswers = new Map<string, string>([
-    ['苏康码颜色*', '绿色'],
-    ['今日是否已做/将做核酸检测*', '是'],
-    [
-        '请注意：根据疾控要求，近6个月有新冠肺炎（确诊/疑似感染/无症状感染）史的人员须在核酸检测中实施单人单管。如你在近6个月内曾有感染史，请在核酸采样时告知采样人员需要单管（校内适用）。*',
-        '已了解知悉。',
-    ],
-    ['你今日的防疫状态是否有异常？*', '无'],
-    [
-        '承诺书*【多选题】',
-        '本人承诺所有填报信息、上传资料均为真实，如有隐瞒、漏报、谎报等行为，或者违反相关规定造成区域恐慌、疾病传播，自愿承担由此导致的相关法律及行政责任。',
-    ],
-    ['当日所在地、隔离防护等防疫状态均无变化*', '无变化，常态化防护中'],
-]);
-
-async function waitForLoading(): Promise<HTMLCollection> {
-    return new Promise(resolve => {
+const waitForLoading: () => Promise<HTMLCollection> = () =>
+    new Promise(resolve => {
         const loadedDiv = document.getElementsByClassName('ui-field-contain');
         if (loadedDiv.length) resolve(loadedDiv);
         else setTimeout(() => resolve(waitForLoading()));
     });
-}
 
 (async () => {
     for (const div of await waitForLoading()) {
         const label = div.getElementsByClassName('field-label')[0];
         const question = label.textContent?.trim() ?? '';
-        const answer = defaultAnswers.get(question);
+        const answer = localStorage.getItem(question);
 
-        if (answer) {
-            const options = [
-                ...div.getElementsByClassName('ui-radio'),
-                ...div.getElementsByClassName('ui-checkbox'),
-            ] as HTMLDivElement[];
+        const options = [
+            ...div.getElementsByClassName('ui-radio'),
+            ...div.getElementsByClassName('ui-checkbox'),
+        ] as HTMLDivElement[];
 
-            for (const option of options)
-                if (option.textContent?.trim() === answer) {
-                    option.click();
-                    break;
-                }
+        for (const option of options) {
+            const optionContent = option.textContent?.trim();
+            if (optionContent === answer) option.click();
+            option.addEventListener('click', () =>
+                localStorage.setItem(question, optionContent!)
+            );
         }
     }
 })();
